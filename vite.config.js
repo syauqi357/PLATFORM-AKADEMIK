d284188dba3748ca8ade54c2470649b25ce90311
syauqi <m.syauqi357@gmail.com>
2026-02-08 21:49:29 +0700

    publish

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PLATFORM-AKADEMIK/',
   plugins: [
       svelte(),
   tailwindcss()
